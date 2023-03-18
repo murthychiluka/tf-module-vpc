b15d180b42c9ee21d927bb82ba95d83f28ce8f8a
Murthy <murthychiluka@gmail.com>
2023-03-18 17:28:33 +0530

    a

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-# resource "aws_default_vpc" "main" {
-#   cidr_block = var.vpc_cidr
-#   tags = {
-#     Name = "Default VPC"
-#   }
-# }
+resource "aws_default_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  
+}
