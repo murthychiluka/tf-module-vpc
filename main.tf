64554ed2378b062ed69320b1bdef0ae42e68a4c7
Murthy <murthychiluka@gmail.com>
2023-03-19 11:40:19 +0530

    main

--- main.tf
+++ main.tf
@@ -16,6 +16,20 @@ resource "aws_subnet" "public_subnet" {
   cidr_block        = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
   
+#Private subnets
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
+ 
+  for_each          = var.private_subnets
+  cidr_block        = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+  
+
 
 }
 
