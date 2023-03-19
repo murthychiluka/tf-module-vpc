997ee15af4e20b0e3c40dd402f197282291756c7
Murthy <murthychiluka@gmail.com>
2023-03-19 20:01:10 +0530

    d

--- main.tf
+++ main.tf
@@ -29,6 +29,11 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
   for_each = var.public_subnets
   tags = merge(
     var.tags,
