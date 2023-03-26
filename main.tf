0c7b0702fcd2f1747c52d8717e60c1700d8f0123
Murthy <murthychiluka@gmail.com>
2023-03-26 16:09:59 +0530

    d

--- main.tf
+++ main.tf
@@ -110,7 +110,10 @@ resource "aws_route_table" "private-route-table" {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
   }
-
+  route {
+    cidr_block                = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
 
 
   tags = merge(
